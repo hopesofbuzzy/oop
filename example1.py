c2301adaac1584f3eec1eac174ad4c9597efae33
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2025-10-23 11:29:37 +0500

    Добавить конфигурации для ruff

---
+++ example1.py
@@ -0,0 +1,12 @@
+class Phone:
+    line_type = 'проводной'
+
+
+rotary_phone = Phone()
+keypad_phone = Phone()
+rotary_phone.line_type = 'Беспроводной'
+
+# Печать содержимого атрибута line_type через объект rotary_phone.
+print(rotary_phone.line_type)
+# Печать содержимого атрибута line_type через объект keypad_phone.
+print(keypad_phone.line_type)
